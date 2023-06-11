ac592dbda7dc7e0a4e69092c21326899a6138002
ajgeiss0702 <ajgeiss72@gmail.com>
2023-06-11 15:50:42 -0700

    Support status and status text in pastebin proxy

--- src/routes/api/pastebin/[code]/+server.ts
+++ src/routes/api/pastebin/[code]/+server.ts
@@ -2,8 +2,10 @@ import type { RequestHandler } from "@sveltejs/kit";
 import { text } from "@sveltejs/kit";
 
 export const GET = (async ({fetch, params}) => {
-  return text(
-    await fetch("https://pastebin.com/raw/" + params.code)
-      .then(r => r.text())
-  );
+  const response = await fetch("https://pastebin.com/raw/" + params.code);
+
+  return text(await response.text(), {
+    status: response.status,
+    statusText: response.statusText
+  });
 }) satisfies RequestHandler;
